506e47159623f8df6e0839022685f517a387646a
Hasan <hasan98209@gmail.com>
2025-01-12 01:35:24 -0600

    added sword anim  + collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+
     [SerializeField] private float moveSpeed = 1f;
 
     private PlayerControls playerControls;
@@ -11,6 +13,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRenderer;
+
+    private bool facingLeft = false;
+
+    
 
     private void Awake()
     {
@@ -41,10 +47,12 @@ public class PlayerController : MonoBehaviour
         if(mousePos.x < playerScreenPoint.x)
         {
             mySpriteRenderer.flipX = true;
+            FacingLeft = true;
         }
         else
         {
             mySpriteRenderer.flipX = false;
+            FacingLeft = false;
         }
     }
     private void PlayerInput()
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    [SerializeField] private GameObject slashAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
+    [SerializeField] private Transform weaponCollider;
+
+    private PlayerControls playerControls;
+    private Animator anim;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private GameObject slashAnim;
+
+    private void Awake()
+    {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        playerControls = new PlayerControls();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+    private void Attack()
+    {
+        anim.SetTrigger("Attack");
+        weaponCollider.gameObject.SetActive(true);
+
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+        slashAnim.transform.parent = this.transform.parent;
+    }
+
+    public void DoneAttackingAnimEvent()
+    {
+        weaponCollider.gameObject.SetActive(false);
+    }
+
+    public void SwingUpFlipAnim()
+    {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
+
+        if(playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    public void SwingDownFlipAnim()
+    {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        if (playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y - playerController.transform.position.y , Mathf.Abs(mousePos.x - playerController.transform.position.x)) * Mathf.Rad2Deg;
+
+        if(mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MouseFollowWithOffset();
+    }
+}
